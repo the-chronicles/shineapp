deba85b44eb2f4b4ffd1355791ab793a5d55fc79
the-chronicles <oluseungideon@gmail.com>
2023-11-27 13:39:45 +0100

    Welcome page

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <Text>Welcome back</Text>
+        <Text>
+          Glad to see you back, please login into your account to continue
+        </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Email Address"/>
+        <TextInput style={styles.textInput} placeholder="Password"/>
+        <Text>Or sign in with</Text>
+      </View>
+      <View>
+        <Button title="Sign in with Google" />
+        <Button title="Sign in with Apple" />
+      </View>
+      <View>
+        <Button title="Continue" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 84,
+    paddingHorizontal: 24,
   },
+  headerText: {
+
+  },
+  inputContainer: {
+    // flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    margin: 20,
+    alignItems: 'center',
+    gap: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#F2F2F2",
+    width: "100%",
+    padding: 14,
+    borderRadius: 15,
+  }
 });
